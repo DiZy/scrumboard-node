d3ec2298699443470bea52f5030f6e6397316bc4
David Zilbrug <davidzilburg@Davids-MacBook-Pro.local>
2017-02-26 12:37:54 -0500

    gulpfile updates, structure fixes, and basic assets

--- gulpfile.js
+++ gulpfile.js
@@ -8,19 +8,29 @@ gulp.task('clean', function() {
     //TODO figure out clean task
 });
 
-gulp.task('css', function() {
-    gulp.src('src/scss/*.scss')
+gulp.task('scss', function() {
+    gulp.src('src/scss/**')
         .pipe(sass())
         .pipe(gulp.dest('build/assets/css'));
 });
 
+gulp.task('css', function() {
+    gulp.src('src/css/**')
+        .pipe(gulp.dest('build/assets/css'));
+});
+
 gulp.task('js', function() {
-    gulp.src('src/nodejs/*.js')
+    gulp.src('src/js/**')
+        .pipe(gulp.dest('build/assets/js'));
+});
+
+gulp.task('nodejs', function() {
+    gulp.src('src/nodejs/**')
         .pipe(gulp.dest('build'));
 });
 
 gulp.task('views', function() {
-    gulp.src('src/views/**/*.ejs')
+    gulp.src('src/views/**')
         .pipe(gulp.dest('build/views'));
 });
 
@@ -34,27 +44,38 @@ gulp.task('supervisor', function() {
     gulpSupervisor('build/app.js');
 });
 
-gulp.task('css-debug', function() {
-    return watch('src/scss/*.scss', { ignoreInitial: false }).pipe(sass())
+gulp.task('scss-debug', function() {
+    return watch('src/scss/**', { ignoreInitial: false }).pipe(sass())
         .pipe(gulp.dest('build/assets/css'));
 });
 
-gulp.task('js-debug', function() {
-    return watch('src/nodejs/*.js', { ignoreInitial: false }).pipe(gulp.dest('build'));
+gulp.task('css-debug', function() {
+    return watch('src/css/**', { ignoreInitial: false })
+        .pipe(gulp.dest('build/assets/css'));
 });
+
+gulp.task('nodejs-debug', function() {
+    return watch('src/nodejs/**', { ignoreInitial: false }).pipe(gulp.dest('build'));
+});
+
+gulp.task('js-debug', function() {
+    return watch('src/js/**', { ignoreInitial: false }).pipe(gulp.dest('build/assets/js'));
+});
+
 
 gulp.task('views-debug', function() {
-    return watch('src/views/**/*.ejs', { ignoreInitial: false }).pipe(gulp.dest('build/views'));
+    return watch('src/views/**', { ignoreInitial: false }).pipe(gulp.dest('build/views'));
 });
 
-gulp.task('default', ['clean', 'css', 'js', 'views', 'run'], function() {
 
-});
-
-gulp.task('build', ['clean', 'css', 'js', 'views'], function() {
+gulp.task('build', ['clean', 'scss', 'css', 'nodejs', 'js', 'views'], function() {
 
 });
 
-gulp.task('debug', ['clean', 'css', 'css-debug', 'js', 'js-debug', 'views', 'views-debug', 'supervisor'], function() {
+gulp.task('default', ['build', 'run'], function() {
+
+});
+
+gulp.task('debug', ['build', 'scss-debug', 'css-debug', 'nodejs-debug', 'js-debug', 'views-debug', 'supervisor'], function() {
 
 });
