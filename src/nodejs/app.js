aa32e2a4b81500038c6dfc702bdc9c4c197b0211
David Zilbrug <davidzilburg@Davids-MacBook-Pro.local>
2017-03-08 20:25:51 -0500

    login functional

--- src/nodejs/app.js
+++ src/nodejs/app.js
@@ -6,6 +6,7 @@ var assert = require('assert');
 var bodyParser = require('body-parser');
 var bcrypt = require('bcryptjs');
 var uuidV4 = require('uuid/v4');
+var session = require('express-session');
 
 //Mongo
 var MongoCollection = require('./modules/MongoCollection');
@@ -18,15 +19,47 @@ app.set('views', path.join(__dirname, '/views'));
 app.use('/assets/', express.static(__dirname + '/assets'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  resave: false, // don't save session if unmodified
+  saveUninitialized: false, // don't create session until something stored
+  secret: 'everything is secret'
+}));
 
-app.get('/', function(req, res) {
+app.get('/logout', function(req, res) {
+	logOut(req);
+    res.redirect('/');
+});
+
+app.get('/', loggedInRedirect, function(req, res) {
     res.render('pages/index');
+});
+
+app.get('/teamhome', requiresLoginRedirect, function(req, res) {
+    res.render('pages/teamhome');
 });
 
 app.post('/signIn', function(req, res) {
 	var username = req.body.username;
 	var password = req.body.password;
-	res.json({type: "error"});
+
+	usersCollection.find({'username' : username}, function(err, results){
+		assert.equal(null, err);
+		if(results.length > 0) {
+			var foundUser = results[0];
+			if(bcrypt.compareSync(password, foundUser.password)){
+				console.log("Succesful login");
+				logIn(req, foundUser._id);
+				return res.json({type: "success"});
+			}
+			else {
+				return res.json({ type: "error", error: "Not a valid username and password combination"});
+			}
+			
+		}
+		else {
+			return res.json({ type: "error", error: "Not a valid username and password combination"});
+		}
+	});
 });
 
 app.post('/signUp', function(req, res) {
@@ -58,7 +91,7 @@ app.post('/signUp', function(req, res) {
 			var salt = bcrypt.genSaltSync(10);
 			password = bcrypt.hashSync(password, salt);
 			var companyId = uuidV4();
-			
+
 			companiesCollection.insert(
 			{"_id": companyId, "name": fullName},
 			function(err, result) {
@@ -78,6 +111,51 @@ app.post('/signUp', function(req, res) {
 	});
 });
 
+
+function requiresLoginRedirect(req, res, next) {
+  if (isLoggedIn(req)) {
+    next();
+  } else {
+    req.session.error = 'Access denied!';
+    res.redirect('/');
+  }
+}
+
+function requiresLogin(req, res, next) {
+  if (isLoggedIn(req)) {
+    next();
+  } else {
+    req.session.error = 'Access denied!';
+    res.json({ type: "error", error: "This requires login."});
+  }
+}
+
+function loggedInRedirect(req, res, next) {
+	if (isLoggedIn(req)) {
+	  res.redirect('/teamhome');
+	} else {
+	  next();
+	}
+}
+
+function isLoggedIn(req) {
+	//TODO: add actual check against db
+	if(req.session.userId) {
+		return true;
+	}
+	return false;
+
+}
+
+function logIn(req, userId) {
+	req.session.userId = userId;
+}
+
+function logOut(req) {
+	req.session.destroy(function(){
+    	console.log("logged out");
+  	});
+}
 
 app.listen(5000);
 console.log("RUNNING ON PORT 5000");
