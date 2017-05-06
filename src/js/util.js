c9b3e68b458d3b17d33b585376c52fb340a6c2ef
David Zilbrug <davidzilburg@Davids-MacBook-Pro.local>
2017-05-06 12:26:30 -0400

    fixed top

--- src/js/util.js
+++ src/js/util.js
@@ -1,3 +1,35 @@
 function internalErrorAlert() {
 	alert("There was an internal server error. Please try again later.");
 }
+
+
+function customAjax(method, url, data, successCallback, failCallback) {
+	$.ajax({
+	    type: method,
+	    url: url,
+	    data: data,
+	    dataType: "json",
+	    contentType: "application/x-www-form-urlencoded"
+
+	})
+	.done(function(data) {
+	    if(data.type == 'success'){
+	    	if(successCallback) {
+	    		successCallback(data);
+	    	}
+	    }
+	    else {
+	    	if(failCallback) {
+	    		failCallback(data);
+	    	}
+	        else {
+	        	alert(data.error);
+	        }
+	    }
+
+	})
+	.fail(function(data) {
+	    alert("Internal Server Error");
+	    console.log(data);
+	});
+}
