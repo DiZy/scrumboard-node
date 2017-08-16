817628cc5cdcd17d635c023a2a6cb03a186aaf17
peter.somers <petersomers2@gmail.com>
2017-08-16 12:54:48 -0400

    -Enchancing test context with "suite" function and "browser" object -Placing webdriverio config options "baseUrl" into the right spot and adding "logLevel" -Adding "logLevel" to config file

--- src/test/support/context.js
+++ src/test/support/context.js
@@ -1,21 +1,56 @@
 const wdio = require('wdio');
 const config = require('../config.json');
+const TEST_TIMEOUT = 10 * 1000;
 
 // This fixes some issues running tests in Intellij
 //     - the need to use the wdio module (and it's wrap function) since Intellij runs tests through mocha rather than through the wdio script
 //     - the increased timeout, which solves this issue
-let _it = it; // just like life
 let mocha = this;
+let _it = it; // just like life
+let _before = before;
+let _beforeEach = beforeEach;
+let _after = after;
+let _afterEach = afterEach;
 
-let browser = wdio.getBrowser({
+browser = wdio.getBrowser({
+    baseUrl: config.baseUrl || 'http://localhost',
     desiredCapabilities: {
         browserName: config.browser || 'chrome',
-        baseUrl: config.baseUrl || 'http://localhost'
-    }
+    },
+    logLevel: config.logLevel || 'silent',
 });
-
-mocha.timeout(10000);
 
 it = function(description, func) {
     _it.call(mocha, description, wdio.wrap(func));
+};
+before = function(func) {
+    _before.call(mocha, wdio.wrap(func));
+};
+beforeEach = function(func) {
+    _beforeEach.call(mocha, wdio.wrap(func));
+};
+after = function(func) {
+    _after.call(mocha, wdio.wrap(func));
+};
+afterEach = function(func) {
+    _afterEach.call(mocha, wdio.wrap(func));
+};
+
+suite = function(description, func) {
+    describe(description, function() {
+        this.timeout(TEST_TIMEOUT);
+
+        // Initialize selenium standalone server if it is not started yet
+        before(wdio.initSelenium);
+
+        before(function() {
+            browser.init();
+        });
+
+        after(function() {
+            browser.end();
+        });
+
+        func();
+    });
 };
